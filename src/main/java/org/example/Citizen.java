f904025bf2a0961883c67cc9b3fa7ed470b209df
Lukerya <l.6808561@gmail.com>
2024-05-26 18:35:54 +0200

    new logic for citizen + checking in simulation

--- src/main/java/org/example/Citizen.java
+++ src/main/java/org/example/Citizen.java
@@ -1,12 +1,34 @@
 package org.example;
+import java.util.Random;
+import java.util.Arrays;
 
-public class Citizen{
-    private int targetImplantNumber;
-
-    public Citizen(GenerateTargetImplantNumber implantNumber) {
-        this.targetImplantNumber = implantNumber.GenerateData();
-    }
+//public class Citizen{
+//    private int targetImplantNumber;
+//
+//    public Citizen(GenerateTargetImplantNumber implantNumber) {
+//        this.targetImplantNumber = implantNumber.GenerateData();
+//    }
 //    public int getTargetImplantNumber() {
 //        return targetImplantNumber;
 //    }
+//}
+
+public class Citizen {
+    private int[] arrayNumberOfImplants;
+    private int size;
+
+    // Konstruktor do inicjalizacji tablicy implantow u citizena
+    public Citizen(GenerateTargetImplantNumber implantNumber) {
+        this.size = implantNumber.GenerateData();
+        this.arrayNumberOfImplants = new int[this.size];
+        fillArrayWithRandomNumbers();
+    }
+    private void fillArrayWithRandomNumbers(){
+        for(int i=0; i < this.size; i++){
+            this.arrayNumberOfImplants[i] = (int) (Math.random() * 101);
+        }
+    }
+    public void printArray() {
+        System.out.println("Item array (size " + this.size + "): " + Arrays.toString(this.arrayNumberOfImplants));
+    }
 }
--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 //import org.apache.poi.ss.usermodel.*;
 //import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 //import java.io.FileOutputStream;
@@ -28,8 +30,19 @@ public class Simulation {
                 .reduce((a, b) -> a + " " + b) // склеивание потока в строку с разделителем
                 .orElse(""); // если поток будет пустой, то возвращает пустую строку
         System.out.println("Generated normal sample: " + formattedGauss);
-        GenerateImplantList city = new GenerateImplantList(10);
-        city.addCitizen();
-        city.printImplants();
+//        GenerateImplantList city = new GenerateImplantList(10);
+//        city.addCitizen();
+//        city.printImplants();
+
+        GenerateTargetImplantNumber implantNumber = new GenerateTargetImplantNumber();
+      //lista dla przechowywania ilosci citizenow
+        List<Citizen> citizen = new ArrayList<>();
+        //zamawiamy na wynos 1000 citizenow i zapraszany do arrsy
+        for (int i = 0; i < 1000; i++){
+            citizen.add(new Citizen(implantNumber));
+        }
+        for (int i = 0; i < citizen.size(); i++) {
+            System.out.println("Citizen" + (i + 1) + ":");
+            citizen.get(i).printArray(); // halo printArray dla i elemeto ( czyli drukujeny spis implantow dla citizena 1 lub 67
     }
-}
+}}
