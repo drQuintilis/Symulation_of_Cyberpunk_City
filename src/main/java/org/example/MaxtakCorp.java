fdc67afb0564255b9a9c7441bed77c01b6528981
dokst <dokstrange42@gmail.com>
2024-06-08 20:29:01 +0200

    MaxtakCorp

--- src/main/java/org/example/MaxtakCorp.java
+++ src/main/java/org/example/MaxtakCorp.java
@@ -1,4 +1,60 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MaxtakCorp {
+    public double moneyFlow;
+    public double costOfActiveUnit;
+    public double costOfHireUnit;
+    private double savedAmount;
+    public List<MaxtakAgent> maxtakAgentList;
+    public Agent agent;
+    public Simulation simulation;
+
+
+    public MaxtakCorp(Simulation simulation, double moneyFlow, double costOfActiveUnit, double costOfHireUnit){
+        this.moneyFlow = moneyFlow;
+        this.costOfActiveUnit = costOfActiveUnit;
+        this.costOfHireUnit = costOfHireUnit;
+        this.savedAmount = 0;
+        this.maxtakAgentList = new LinkedList<>();
+        this.simulation = simulation;
+    }
+
+    public void doIncomeUpdate() {
+        this.savedAmount += this.moneyFlow;
+        this.savedAmount -= this.costOfActiveUnit * this.maxtakAgentList.size();
+    }
+
+    public void doUpdateAgents(){
+        for(int i = 0; ; i++){
+            if(this.savedAmount > this.costOfHireUnit && this.moneyFlow
+                    > (this.costOfActiveUnit * (this.maxtakAgentList.size() + 1))){
+                this.maxtakAgentList.add(new MaxtakAgent(this.agent.agentID));
+                this.savedAmount -= this.costOfHireUnit;
+            } else break;
+        }
+    }
+
+    public void doTick(String step){
+        doIncomeUpdate();
+        doUpdateAgents();
+    }
+
+    @Override
+    public String toString() {
+        String arrayOfMaxtakAgents = "";
+        for(MaxtakAgent maxtakAgent: this.maxtakAgentList){
+            if(maxtakAgent != null){
+                arrayOfMaxtakAgents = arrayOfMaxtakAgents + maxtakAgent.toString();
+            }
+        }
+        return "MaxtakCorp date:" + "\n" +
+                "Money flow: " + this.moneyFlow + "\n" +
+                "Cost of active unit: " + this.costOfActiveUnit +"\n" +
+                "Cost of hire unit: " + this.costOfHireUnit +"\n" +
+                "Saved amount: " + this.savedAmount + "\n" +
+                "Array of Maxtak agents: " + arrayOfMaxtakAgents;
+    }
 }
